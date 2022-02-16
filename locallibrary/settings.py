f0aea0e47e72d66cf483de749e90ac0f68a76e37
Majid Benam <benam.mjd@gmail.com>
2022-02-16 15:50:59 +0100

    Added django-heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+django_heroku.settings(locals())

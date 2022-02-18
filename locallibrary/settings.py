e3a8662f8ea7f21cec046b6dddf6bbb965c5eaed
Majid Benam <benam.mjd@gmail.com>
2022-02-18 14:18:00 +0100

    updates settings for S3 connection again

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -178,13 +178,23 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
+# easily checkable (this is where we put things on aws)
+AWS_LOCATION = 'staticfiles'
+
+# this might be good:
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static')
+#     ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles'),
+#     os.path.join(BASE_DIR, 'catalog/static'),
 # ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # potentially switchable to django_on_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(),  staticfiles=False)
